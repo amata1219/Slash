3510140e7a2f5881b042dfa25b0b382e081145f0
amata1219 <suzukiamata@gmail.com>
2019-12-27 13:17:31 +0900

    こみっとー

--- src/amata1219/slash/BranchedExecutorBuilder.java
+++ src/amata1219/slash/BranchedExecutorBuilder.java
@@ -1,19 +1,40 @@
 package amata1219.slash;
 
+import java.util.HashMap;
+
 import org.bukkit.command.CommandSender;
 
 public class BranchedExecutorBuilder<S extends CommandSender> {
 	
-	/*
-	 * BranchedExecutorBuilder.builder(
-	 *   whenArgumentsInsufficient,
-	 *   whenBranchNotFound,
-	 * )
-	 * .branch(executor1, "exe", "ex", "e")
-	 * .branch()
-	 * .branch()
-	 * .build()
-	 * 
-	 */
+	public static <S extends CommandSender> BranchedExecutorBuilder<S> builder(){
+		return new BranchedExecutorBuilder<>();
+	}
+	
+	private String whenArgumentsInsufficient, whenBranchNotFound;
+	private HashMap<String, PartialExecutor<S>> branches = new HashMap<>();
+	
+	public BranchedExecutorBuilder<S> whenArgumentsInsufficient(CharSequence error){
+		whenArgumentsInsufficient = error.toString();
+		return this;
+	}
+	
+	public BranchedExecutorBuilder<S> whenBranchNotFound(CharSequence error){
+		whenBranchNotFound = error.toString();
+		return this;
+	}
+	
+	public BranchedExecutorBuilder<S> branch(PartialExecutor<S> executor, String argument, String... aliases){
+		branches.put(argument, executor);
+		for(String alias : aliases) branches.put(alias, executor);
+		return this;
+	}
+	
+	public PartialExecutor<S> build(){
+		return (sender, arguments) -> {
+			if(arguments.isEmpty()) sender.sendMessage(whenArgumentsInsufficient);
+			else if(branches.containsKey(arguments.element())) branches.get(arguments.poll()).execute(sender, arguments);
+			else sender.sendMessage(whenBranchNotFound);
+		};
+	}
 
 }
